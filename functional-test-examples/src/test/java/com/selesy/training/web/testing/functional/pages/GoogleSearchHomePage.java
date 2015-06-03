e7f0fd9a1ade170c38c0e528132e48ea8f376169
Steve Moyer <smoyer1@selesy.com>
2015-06-02 21:09:26 -0400

    Added javadocs comments to the two Google page objects.

--- functional-test-examples/src/test/java/com/selesy/training/web/testing/functional/pages/GoogleSearchHomePage.java
+++ functional-test-examples/src/test/java/com/selesy/training/web/testing/functional/pages/GoogleSearchHomePage.java
@@ -7,15 +7,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+/**
+ * Provides access to the elements of the Google Search Home Page needed to
+ * execute the test cases.
+ * 
+ * @author Steve Moyer
+ */
 public class GoogleSearchHomePage extends AbstractPageObject {
   
   String searchTerms;
   
+  /**
+   * Connects the provided WebDriver to the Google search page and constructs
+   * a new instance of the GoogleSearchHomePage class.
+   * 
+   * @param webDriver the current instance of the WebDriver.
+   */
   public GoogleSearchHomePage(WebDriver webDriver) {
     super(webDriver);
     webDriver.get("https://www.google.com");
   }
   
+  /**
+   * Clicks the search button on the Google search home page.
+   * 
+   * @return an instance of the resulting GoogleSearchResultsPage.
+   */
   public GoogleSearchResultsPage clickGoogleSearchButton() {
     WebElement element = null;
     try{
@@ -38,10 +55,23 @@ public class GoogleSearchHomePage extends AbstractPageObject {
     return false;
   }
   
+  /**
+   * Verifies that the actual title (retrieved via the WebDriver) to the
+   * expected titled passed as the only parameter.
+   * 
+   * @param title the expected title of the current web page.
+   * @return a boolean true value if the retrieved title matches the expected
+   *         title, otherwise false.
+   */
   public boolean isTitled(String title) {
     return webDriver.getTitle().toLowerCase().startsWith(title.toLowerCase());
   }
   
+  /**
+   * Enters the search terms into the Google Search page's search field.
+   * 
+   * @param searchTerms the terms to be searched.
+   */
   public void setSearchTerms(String searchTerms) {
     this.searchTerms = searchTerms;
     
--- functional-test-examples/src/test/java/com/selesy/training/web/testing/functional/pages/GoogleSearchResultsPage.java
+++ functional-test-examples/src/test/java/com/selesy/training/web/testing/functional/pages/GoogleSearchResultsPage.java
@@ -2,8 +2,22 @@ package com.selesy.training.web.testing.functional.pages;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Provides methods needed to interogate the state of the Google search results
+ * page.
+ * 
+ * @author Steve Moyer <smoyer1@selesy.com>
+ */
 public class GoogleSearchResultsPage extends AbstractPageObject {
 
+  /**
+   * Constructs a Google search results page object using the current state
+   * of the WebDriver provided as a parameter.  Since there is no call to a
+   * URL, this constructor should only be called by clicking the search button
+   * (using the appropriate method in the GoogleSearchHomePage).
+   * 
+   * @param webDriver the current test's WebDriver instance.
+   */
   public GoogleSearchResultsPage(WebDriver webDriver) {
     super(webDriver);
   }
